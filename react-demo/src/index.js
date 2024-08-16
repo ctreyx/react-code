abb1c6b68ba12d25aab2af14fe80454cff00478b
fumi <330696896@qq.com>
2024-08-16 11:18:24 +0800

    完成第一次render渲染

--- react-demo/src/index.js
+++ react-demo/src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
+const jsx = (<div>
+    <span>jsx</span>
+</div>)
 
-const jsx = <div>hell <span>jsx</span></div>
+const root = document.querySelector('#root');
+
+ReactDOM.createRoot(root).render(jsx);
+
 console.log(React);
 
 console.log(jsx);
+
+console.log(ReactDOM);
